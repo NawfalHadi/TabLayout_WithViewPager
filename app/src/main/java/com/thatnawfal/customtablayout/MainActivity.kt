517919e096b7e65669318205d7fcf0da7328c32f
Nawfal Hadi <nawfalhdi@gmail.com>
2022-11-12 21:35:59 +0800

    Initial commit

---
+++ app/src/main/java/com/thatnawfal/customtablayout/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.thatnawfal.customtablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private val TAB_TITLES = intArrayOf(
+            R.string.home_titles,
+            R.string.profile_titles
+        )
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionPagerAdapter(this)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionPagerAdapter
+
+        val tabs: TabLayout = findViewById(R.id.tabs)
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
+    }
+
+}
